69d4140853a34124cb595a0bb8857a309c0a3a53
Irina Khorunz <irahorunz87@gmail.com>
2023-10-06 22:22:25 +0300

    add selectors

---
+++ src/zustand/selectors.js
@@ -0,0 +1,23 @@
+export const getFetchRecipes = state => state.fetchRecipes;
+
+export const getRecipes = state => state.recipes;
+
+export const getRecipesLength = state => state.recipes.length;
+
+export const getLazyLoadBeer = state => state.lazyLoadBeer;
+
+export const getPage = state => state.page;
+
+export const getLoadMoreRecipes = state => state.loadMoreRecipes;
+
+export const getSelectedCard = state => state.selectedCard;
+
+export const getSelectedCardLength = state => state.selectedCard.length;
+
+export const getToggleBeerCards = state => state.toggleBeerCards;
+
+export const getDeleteRecipes = state => state.deleteRecipes;
+
+export const getBeerRecipeById = state => state.getRecipeById;
+
+export const getSelectedRecipe = state => state.selectedRecipe;
