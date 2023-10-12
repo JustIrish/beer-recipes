2af20356a43030dc0e64a4533e8f49294941e835
Irina Khorunz <irahorunz87@gmail.com>
2023-10-12 22:36:07 +0300

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 
+import useBeerStore from '../zustand/store';
+
 import SharedLayout from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/HomePage'));
 const RecipeDetails = lazy(() => import('../pages/RecipeDetails'));
+
+useBeerStore.getState().fetchRecipes(1);
 
 function App() {
   return (
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,20 @@
-import { useEffect } from 'react';
 // import { toast } from 'react-hot-toast';
 import useBeerStore from '../zustand/store';
-import {
-  getFetchRecipes,
-  getPage,
-  getRecipesLength,
-  getSelectedCardLength,
-} from '../zustand/selectors';
+import { getRecipesLength, getSelectedCardLength } from '../zustand/selectors';
 
 import RecipesList from '../components/RecipesList/RecipesList';
 import DeleteBtn from '../components/DeleteBtn/DeleteBtn';
 import { HomeTitle } from './HomePages.styled';
 
 const Home = () => {
-  const fetch = useBeerStore(getFetchRecipes);
   const recipesLength = useBeerStore(getRecipesLength);
-  const page = useBeerStore(getPage);
   const isSelectedCard = useBeerStore(getSelectedCardLength);
-
-  useEffect(() => {
-    fetch(page);
-  }, []);
 
   return (
     <>
       <HomeTitle>Beer recipes</HomeTitle>
-      {recipesLength && <RecipesList />}
-      {isSelectedCard && <DeleteBtn />}
+      {recipesLength > 0 && <RecipesList />}
+      {!!isSelectedCard && <DeleteBtn />}
     </>
   );
 };
