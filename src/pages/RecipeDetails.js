7d9fec8b57244e83787cb5fc2f9b30061a277086
Irina Khorunz <irahorunz87@gmail.com>
2023-10-13 22:38:24 +0300

    edit RecipeDetails

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,13 +1,15 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { HiArrowLeft } from 'react-icons/hi';
 
 import useBeerStore from '../zustand/store';
-import { getSelectedRecipe } from '../zustand/selectors';
 
 import { Wrap, DescWrap } from './RecipesDetails.styles';
 
-const MovieDetails = () => {
-  const selectedRecipe = useBeerStore(getSelectedRecipe);
+const RecipeDetails = () => {
+  const { recipeId } = useParams();
+  const { recipes } = useBeerStore();
+
+  const recipe = recipes.find(({ id }) => id === Number(recipeId));
 
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/';
@@ -17,23 +19,30 @@ const MovieDetails = () => {
       <Link to={backLinkHref}>
         <HiArrowLeft size="25" color=" #15253c" />
       </Link>
-      <Wrap>
-        <img
-          src={selectedRecipe.image_url}
-          alt="beer"
-          width={100}
-          loading="lazy"
-        />
-        <DescWrap>
-          <p>{selectedRecipe.name}</p>
-          <p>{selectedRecipe.tagline}</p>
-          <p>First brewed: {selectedRecipe.first_brewed}</p>
-          <p>Description: {selectedRecipe.description}</p>
-          {/* <p>Ingredients{selectedRecipe.ingredients.mult}</p> */}
-        </DescWrap>
-      </Wrap>
+      {recipe ? (
+        <Wrap>
+          <img src={recipe.image_url} alt="beer" width={100} loading="lazy" />
+          <DescWrap>
+            <p>{recipe.name}</p>
+            <p>{recipe.tagline}</p>
+            <p>First brewed: {recipe.first_brewed}</p>
+            <p>Description: {recipe.description}</p>
+            {/* <p>Ingredients{ingredients.mult}</p> */}
+          </DescWrap>
+        </Wrap>
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            fontSize: '20px',
+          }}
+        >
+          Recipe not found.
+        </div>
+      )}
     </>
   );
 };
 
-export default MovieDetails;
+export default RecipeDetails;
